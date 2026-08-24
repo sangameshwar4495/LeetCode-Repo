c229da6b55f8ceabd218446aabfa0f5fb2d9ef01
sangameshwar4495 <168275282+sangameshwar4495@users.noreply.github.com>
2026-08-24 23:54:29 +0530

    Time: 0 ms (100%), Space: 15.1 MB (21.79%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,16 +11,27 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, int vd, vector<int>& ans){
-        if(root==NULL) return;
-        if(ans.size()==vd) ans.push_back(root->val);
-        else ans[vd] = root->val;
-        dfs(root->left, vd+1, ans);
-        dfs(root->right, vd+1, ans);
-    }
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        dfs(root, 0, ans);
+        if(root==NULL) return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        unordered_map<int, int> m;
+        int vd = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz-->0){
+                TreeNode* curr = q.front();
+                q.pop();
+                m[vd] = curr->val;
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            vd++;
+        }
+        vector<int> ans(m.size());
+        for(auto p:m){
+            ans[p.first] = p.second;
+        }
         return ans;
     }
 };
