4f06b652e2d00b9aee4c9d155ddfcfc1eb59a6d5
sangameshwar4495 <168275282+sangameshwar4495@users.noreply.github.com>
2026-08-26 11:16:30 +0530

    Time: 0 ms (100%), Space: 8.9 MB (45.09%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    bool isSmall(string s1, string s2){
-        if(s1.size()<s2.size()) return true;
-        if(s1.size()>s2.size()) return false;
-        for(int i=0; i<s1.size(); i++){
-            if(s1[i]>s2[i]) return false;
-            else if(s1[i]<s2[i]) return true;
-        }
-        return true;
-    }
+
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
         int minL = n;
@@ -25,11 +17,12 @@ public:
             }
 
             // ones==k
-            if(minL>=(r-l+1)){
+            if(minL>(r-l+1)){
                 minL= r-l+1;
-                string currString = s.substr(l,r-l+1);
-                cout<<"doing here\n"<<currString<<"\n";
-                if(ans=="" || isSmall(currString,ans)) ans = currString;
+                ans =s.substr(l,r-l+1);
+            }else if(minL==(r-l+1)){
+                string curr = s.substr(l,r-l+1);  
+                if(ans=="" || ans>curr) ans = curr;
             }
         }
         return ans;
