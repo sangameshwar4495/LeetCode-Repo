e157fd41e8735ffded586583f6d2db91699bbafa
sangameshwar4495 <168275282+sangameshwar4495@users.noreply.github.com>
2026-08-28 16:18:06 +0530

    Time: 36 ms (11.31%), Space: 25.1 MB (11.31%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,75 +1,50 @@
 class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
-        vector<int> cnt(26, 0);
-        for (char c : s) {
-            cnt[c - 'a']++;
+        vector<int> cnt(26,0);
+        bool greaterfound = false;
+        for(char ch: s){
+            cnt[ch-'a']++;
         }
-
-        string res;
         int n = target.size();
-        for (int i = 0; i < n; i++) {
-            int targetChar = target[i] - 'a';
-
-            // Case 1: First try to place the same character as target[i] at the
-            // current position
-            if (cnt[targetChar] > 0) {
-                cnt[targetChar]--;
-                // Check if the remaining characters can form a string greater
-                // than target[i+1:]
-                if (canFormGreater(cnt, target, i + 1)) {
-                    res.push_back(target[i]);
-                    continue;
-                }
-                // Cannot form a larger string, backtrack
-                cnt[targetChar]++;
-            }
-
-            // Case 2: Place a character greater than target[i] at the current
-            // position
-            for (int j = targetChar + 1; j < 26; j++) {
-                if (cnt[j] > 0) {
+        string ans;
+        for(int i=0;i<n; i++){
+            for(int j=target[i]-'a'; j<26; j++){
+                if(cnt[j]>0){
                     cnt[j]--;
-                    res.push_back('a' + j);
-                    // Fill remaining positions with the smallest
-                    // lexicographical order
-                    res += getMinString(cnt);
-                    return res;
+                    ans.push_back('a'+j);
+                    if(isPossible(ans,target, cnt)){
+                        if(j>(target[i]-'a')){
+                             greaterfound = true;
+                            cout<<greaterfound<<endl;
+                        }
+                        break;
+                    }
+                    else{
+                        cnt[j]++;
+                        ans.pop_back();
+                    } 
                 }
             }
-
-            // No feasible solution found, return directly
-            return "";
+            if(greaterfound) break;
         }
-
-        return "";
-    }
-
-private:
-    // Check if the remaining characters can form a string greater than the
-    // suffix.
-    bool canFormGreater(const vector<int>& cnt, const string& target,
-                        int start) {
-        string maxStr = getMaxString(cnt);
-        string suffix = target.substr(start);
-        return maxStr > suffix;
-    }
-
-    // Get the maximum lexicographical string (in descending order)
-    string getMaxString(const vector<int>& cnt) {
-        string res;
-        for (int i = 25; i >= 0; i--) {
-            res.append(cnt[i], 'a' + i);
+        if(greaterfound)
+        for(int i=0; i<26; i++){
+            while(cnt[i]>0){
+                ans.push_back('a'+i);
+                cnt[i]--;
+            }
         }
-        return res;
-    }
+        return ans;
 
-    // Get the lexicographically smallest string (in ascending order)
-    string getMinString(const vector<int>& cnt) {
-        string res;
-        for (int i = 0; i < 26; i++) {
-            res.append(cnt[i], 'a' + i);
+    }
+    bool isPossible(string ans,string &target, vector<int> cnt){
+        for(int i=25; i>=0; i--){
+            while(cnt[i]>0){
+                ans.push_back('a'+i);
+                cnt[i]--;
+            }
         }
-        return res;
+        return ans>target;
     }
 };
