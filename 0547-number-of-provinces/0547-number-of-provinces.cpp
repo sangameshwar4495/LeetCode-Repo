bf91e9518ffa770944b5c495aaeea0c82bfa126d
sangameshwar4495 <168275282+sangameshwar4495@users.noreply.github.com>
2026-09-02 15:21:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (82.3%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    
-    int find(int x, vector<int> &par){
-        if(par[x]==x) return x;
-        return par[x] = find(par[x], par);
-    }
-    void unionByRank(int u, int v, vector<int>& rank, vector<int>& par){
-        int paru =  find(u, par);
-        int parv = find(v, par);
-
-        if(rank[paru]==rank[parv]){
-            rank[paru]++;
-            par[parv] = paru;
-        }else if(rank[paru]>rank[parv]){
-            par[parv] = paru;
-        }else par[paru] = parv;
-     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> rank(n,0);
-        vector<int> par(n);
-        for(int i=0; i<n; i++){
-            par[i]=i;
-        }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(isConnected[i][j]==1){
-                    unionByRank(i,j, rank, par);
-                }
+    void dfs(int i, vector<vector<int>>& isConnected, vector<bool> &vis){
+        vis[i] = 1;
+        for(int j=0; j<vis.size(); j++){
+            if(isConnected[i][j]==1){
+                if(!vis[j]) dfs(j, isConnected, vis);
             }
         }
-        unordered_set<int> s;
-        for(int ele: par) {
-            int parele = find(ele,par);
-            if(!s.count(parele)) s.insert(parele);}
-        return s.size();
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> vis(n, false);
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                ans++;
+                dfs(i, isConnected, vis);
+            }
+        }
+        return ans;
     }
 };
